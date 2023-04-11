e24ebef7b0acdbedcede37f32fbc45022aa3772c
ViktorOsu <123021878+ViktorOsu@users.noreply.github.com>
2023-04-12 00:00:01 +0300

    Home Work 11 with devices

--- src/index.js
+++ src/index.js
@@ -6,17 +6,43 @@ import { successInfo, specificInfo, warningInfo } from './js/notify';
 
 import { ImagesApi } from './js/images-api';
 
-// ImagesAPI.getImages('cat');
-
 const searchFormEl = document.querySelector('.search-form');
 const galleryList = document.querySelector('.gallery-list');
 const loadMoreBtnEl = document.querySelector('.load-more');
+const intersectionTargetEl = document.querySelector('.observer');
 
 const imagesApi = new ImagesApi();
-
 let isShow = 0;
 
-// console.log(imagesApi);
+loadMoreBtnEl.classList.add('is-hidden');
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    if (entries[0].isIntersecting) {
+      imagesApi.page += 1;
+
+      imagesApi.fetchPhotosByQuery().then(data => {
+        galleryList.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+
+        let gallery = new SimpleLightbox('.gallery a', {
+          captionsData: 'alt',
+          captionDelay: 250,
+        });
+
+        gallery.refresh();
+
+        if (imagesApi.page === data.totalHits) {
+          observer.unobserve(intersectionTargetEl);
+        }
+      });
+    }
+  },
+  {
+    root: null,
+    rootMargin: '0px 0px 100px 0px',
+    threshold: 1,
+  }
+);
 
 const onSearchFormSubmit = async event => {
   event.preventDefault();
@@ -26,7 +52,6 @@ const onSearchFormSubmit = async event => {
   imagesApi.query = formEl.elements.search_query.value;
   imagesApi.page = 1;
 
-  // console.log(imagesApi.query);
   isShow += 40;
   try {
     const data = await imagesApi.fetchPhotosByQuery();
@@ -39,11 +64,11 @@ const onSearchFormSubmit = async event => {
 
     if (data.total === 0) {
       galleryList.innerHTML = '';
-      loadMoreBtnEl.classList.add('is-hidden');
+      // loadMoreBtnEl.classList.add('is-hidden');
       throw new Error(
         'Sorry, there are no images matching your search query. Please try again.'
       );
-      loadMoreBtnEl.classList.add('is-hidden');
+      // loadMoreBtnEl.classList.add('is-hidden');
       return;
     }
 
@@ -52,22 +77,34 @@ const onSearchFormSubmit = async event => {
     if (data.totalHits === 1) {
       galleryList.innerHTML = createMarkup(data.hits);
       gallery.refresh();
-      loadMoreBtnEl.classList.add('is-hidden');
+      // loadMoreBtnEl.classList.add('is-hidden');
       return;
     }
 
-    loadMoreBtnEl.classList.remove('is-hidden');
+    // loadMoreBtnEl.classList.remove('is-hidden');
     successInfo(data);
     // Notify.success(`Hooray! We found ${data.totalHits} images.`);
     galleryList.innerHTML = createMarkup(data.hits);
 
     gallery.refresh();
 
-    loadMoreBtnEl.classList.remove('is-hidden');
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      behavior: 'smooth',
+    });
+
+    // loadMoreBtnEl.classList.remove('is-hidden');
+
+    setTimeout(() => {
+      observer.observe(intersectionTargetEl);
+    }, 300);
 
     if (isShow >= data.totalHits) {
       console.log(isShow);
-      loadMoreBtnEl.classList.add('is-hidden');
+      // loadMoreBtnEl.classList.add('is-hidden');
       specificInfo();
       // Notify.info(`We're sorry, but you've reached the end of search results.`);
     }
@@ -77,26 +114,26 @@ const onSearchFormSubmit = async event => {
   }
 };
 
-const onLoadMoreBtnClick = async event => {
-  imagesApi.page += 1;
+// const onLoadMoreBtnClick = async event => {
+//   imagesApi.page += 1;
 
-  console.log(isShow);
+//   console.log(isShow);
 
-  try {
-    const data = await imagesApi.fetchPhotosByQuery();
+//   try {
+//     const data = await imagesApi.fetchPhotosByQuery();
 
-    galleryList.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+//     galleryList.insertAdjacentHTML('beforeend', createMarkup(data.hits));
 
-    let gallery = new SimpleLightbox('.gallery a', {
-      captionsData: 'alt',
-      captionDelay: 250,
-    });
+//     let gallery = new SimpleLightbox('.gallery a', {
+//       captionsData: 'alt',
+//       captionDelay: 250,
+//     });
 
-    gallery.refresh();
-  } catch (error) {
-    console.log(err);
-  }
-};
+//     gallery.refresh();
+//   } catch (error) {
+//     console.log(err);
+//   }
+// };
 
 let gallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
@@ -104,6 +141,4 @@ let gallery = new SimpleLightbox('.gallery a', {
 });
 
 searchFormEl.addEventListener('submit', onSearchFormSubmit);
-loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick);
-
-console.log(imagesApi);
+// loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick);
